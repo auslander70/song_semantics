43293f0773cd2cf22b76b5689ec687a5891be9c6
Wil DAmato <wil.damato@gmail.com>
2016-02-25 18:38:39 +0000

    added lyrics table def, sqlalchemy relationships

--- models.py
+++ models.py
@@ -6,10 +6,12 @@ from tuneful import app
 from .database import Base, engine
 
 class Song(Base):
-  __tablename__ = 'songs'
+  __tablename__ = 'song'
   id = Column(Integer, primary_key=True)
   title = Column(String(256))
   artist = Column(String(256))
+  lyrics = relationship('Lyrics', uselist=False, backref='song')
+  charts = relationship('ChartSongs', backref='song')
   def as_dictionary(self):
     return {
       'id': self.id,
@@ -19,9 +21,10 @@ class Song(Base):
   
 
 class Chart(Base):
-  __tablename__ = 'charts'
+  __tablename__ = 'chart'
   id = Column(Integer, primary_key=True)
   date = Column(Date)
+  songs = relationship('Song', secondary='chart_chartsongs_rel', backref=songs)
   def as_dictionary(self):
     return {
       'id': self.id, 
@@ -33,10 +36,25 @@ class ChartSongs(Base):
   __tablename__ = 'chartsongs'
   id = Column(Integer, primary_key=True)
   rank = Column(Integer)
+  song_id = Column(Integer, ForeignKey('song.id'), nullable=False)
+  chart_id = Column(Integer, ForeignKey('chart.id'), nullable=False)
   def as_dictionary(self):
     return {
       'id': self.id,
       'rank': self.ranks
     }
     
+chart_chartsongs_rel = Table('chart_chartsongs_rel', Base.metadata, 
+  Column('chart_id', Integer, ForeignKey('chart.id')),
+  Column('chartsongs_id', Integer, ForeignKey('chargsongs.id'))
+)
     
+class Lyrics(Base):
+  __tablename__ = 'lyrics'
+  id = Column(Integer, primary_key=True)
+  lyrics = Column(Text)
+  song_id = Column(Integer, ForeignKey('song.id'), nullable=False)
+  def as_dictionary(self):
+    'id': self.id,
+    'lyrics': self.lyrics
+  }
