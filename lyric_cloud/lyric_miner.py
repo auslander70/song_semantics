f8e2f216b5a6e468f554ae86f2cc178b6e083b18
Wil DAmato <wil.damato@gmail.com>
2016-02-27 16:18:04 +0000

    added RemoveLeadingArticle function, more url generation

--- lyric_cloud/lyric_miner.py
+++ lyric_cloud/lyric_miner.py
@@ -12,13 +12,21 @@ from lyric_cloud import models
 from lyric_cloud.chart_miner import GetURL, SongExists
 
 def GenerateLyricsUrl(title, artist):
+  # TODO: remove inital 'The' from artist names
   protocol = 'http://'
   base_url = 'www.lyricsmode.com'
   artist_initial = artist[0]
+  artist = RemoveLeadingArticle('The', artist, ' ')
   artist_url = artist.replace(' ','_')
   title_url = title.replace(' ','_')
   url = protocol + base_url + '/' + artist_initial + '/' + artist_url + '/' + title_url + '.html'
-  return url
+  return url.lower()
+  
+def RemoveLeadingArticle(article, fragment, separator):
+  fraglist = fragment.split(separator)
+  fraglist[0] = fraglist[0].replace(article, '')
+  fragment = ' '.join(fraglist).strip()
+  return fragment
   
 def GetLyrics(title, artist):
   pass
